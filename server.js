711c148e9772f230b5eaa95b8aa8c900012dbf39
smartripplanner <smartripplanner@gmail.com>
2026-03-30 20:59:50 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -10,13 +10,7 @@ const dns = require("dns");
 dns.setDefaultResultOrder('ipv4first');
 
 const app = express();
-const compression = require("compression");
-app.use(compression());
 
-app.use((req, res, next) => {
-    res.setTimeout(120000);
-    next();
-});
 // 1. Enable CORS for all routes
 app.use(cors());
 
